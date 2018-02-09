287cbd82cbc15520ec4f587f23244a84f6fb9398
Hedviga <hedv0147@edu.eal.dk>
2018-02-09 12:21:49 +0100

    a

--- App/app/src/main/java/com/example/user/myfavoritethingsapp/MainActivity.java
+++ App/app/src/main/java/com/example/user/myfavoritethingsapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.user.myfavoritethingsapp;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +11,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     public TextView myFaceText;
     public ImageButton myFaceButton;
+    public Handler h = new Handler(){};
+    //public Thread thrd;  //thread didnt work, but just in case i will leave it
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,9 +20,21 @@ public class MainActivity extends AppCompatActivity {
         myFaceText = (TextView) findViewById(R.id.myFaceTxt);
         myFaceButton = (ImageButton)findViewById(R.id.myFace);//to find the button
         myFaceButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){//whatever button dose, and it must be called onClick with small o
-                myFaceText.setText("You clicked it!");
-            }
+            public void onClick(View v){//whatever button does, and it must be called onClick with small o
+                //thrd = new Thread() {
+                myFaceText.setText("You pressed it!");
+                boolean b = h.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+
+
+
+                        //thrd.sleep(300);
+                        Intent moveToNewActivity = new Intent(MainActivity.this, Menu.class);
+                        startActivity(moveToNewActivity);//}; }
+                    }
+                },1000);
+            };
         });
     }
 }
